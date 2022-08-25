a6d04ab6e302e01d208eb4d738346060a0a5c9f1
MliKiowa <nineto0@163.com>
2022-08-25 15:30:10 +0800

    Create index.js

---
+++ scripts/generate/index.js
@@ -0,0 +1,38 @@
+'use strict';
+hexo.extend.generator.register('index', function(locals) {
+  let covers = [];
+  let catlist = [];
+  let pages = [];
+  const config = hexo.config;
+  const theme = hexo.theme.config;
+  const sticky = locals.posts.find({'sticky': true}).sort(config.index_generator.order_by);
+  const posts = locals.posts.find({'sticky': {$exists: false}}).sort(config.index_generator.order_by);
+  const paginationDir = config.pagination_dir || 'page';
+  const path = config.index_generator.path || '';
+  const categories = locals.categories;
+  console.log(sticky);
+  if(posts.length > 0) {
+      pages = pagination(path, posts, {
+      perPage: config.index_generator.per_page,
+      layout: ['index', 'archive'],
+      format: paginationDir + '/%d/',
+      data: {
+        __index: true,
+        catlist: catlist,
+        sticky: sticky
+      }
+    });
+  } else {
+    pages = [{
+        path,
+        layout: ['index', 'archive'],
+        data: {
+          __index: true,
+          catlist: catlist,
+          sticky: sticky
+        }
+      }];
+  }
+  return [...covers, ...pages]; 
+
+}
